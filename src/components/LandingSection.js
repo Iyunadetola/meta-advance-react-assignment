9801cf9f92cc3c087994728e674052bf4a1dd3e9
tee <aderetioluwatoyin@gmail.com>
2023-09-08 21:48:31 +0100

    first commit

---
+++ src/components/LandingSection.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Avatar, Heading, VStack } from "@chakra-ui/react";
+import FullScreenSection from "./FullScreenSection";
+
+const greeting = "Hello, I am Toyin!";
+const bio1 = "A frontend developer";
+const bio2 = "specialised in React";
+
+// Implement the UI for the LandingSection component according to the instructions.
+// Use a combination of Avatar, Heading and VStack components.
+const LandingSection = () => (
+  <FullScreenSection
+    justifyContent="center"
+    alignItems="center"
+    isDarkBackground
+    backgroundColor="#2A4365"
+  >
+    <VStack spacing={16}>
+      <Avatar src='https://i.pravatar.cc/150?img=7' size='2x1' alt='Toyin Salami' />
+      <Heading as='h4' size='md' noOfliners={1}>{greeting} </Heading>
+
+      <VStack >
+        <Heading as='h1' size='3x1' noOfLines={1} >{bio1} </Heading>
+
+        <Heading as='h1' size='3x1' noOfLines={1} >{bio2} </Heading>
+      </VStack>
+    </VStack>
+
+
+  </FullScreenSection>
+);
+
+export default LandingSection;
